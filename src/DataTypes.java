52171ba7cf3c852033e9373fe546e4042d439fa2
Hrushikesh R Patil <hrushi0706@gmail.com>
2025-04-05 10:56:09 +0530

    #6 Data Types

---
+++ src/DataTypes.java
@@ -0,0 +1,29 @@
+package src;
+
+public class DataTypes
+{
+    public static  void main(String a[])
+    {
+        byte by = 127;
+        int i = 127;
+        short sh = 127;
+        long l = 127L;
+
+        float f = 127.0f;
+        double d = 127.0d;
+
+        char c = 'a';
+
+        boolean b = true;
+
+        System.out.println(by);
+        System.out.println(i);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+
+    }
+}
