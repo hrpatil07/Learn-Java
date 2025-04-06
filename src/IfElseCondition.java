11205acf8730827e53fab1fcb25ef4eb7e0da3c5
Hrushikesh R Patil <hrushi0706@gmail.com>
2025-04-06 10:16:43 +0530

    #12:IF Else condition

---
+++ src/IfElseCondition.java
@@ -0,0 +1,16 @@
+package src;
+
+public class IfElseCondition
+{
+    public  static  void main(String[] args)
+    {
+            int x = 10;
+            int y = 20;
+            System.out.println("x > y");
+            if(x > y){
+                System.out.println("x is greater than y");
+            }else{
+                System.out.println("x is less than y");
+            }
+    }
+}
